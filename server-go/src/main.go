177e5416db5f5d38227b30467ddd6ca3662a8e4e
zhoujinlu <47242868@163.com>
2018-12-05 16:50:17 +0800

    simple server client test

--- server-go/src/main.go
+++ server-go/src/main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"fmt"
+	"net"
 )
+
+const VERSION = "1.0.0"
+const PORT = ":8085"
 
 func main() {
 	fmt.Println("Hello world")
+	linster, err := net.Listen("tcp", PORT)
+	if err != nil {
+		fmt.Println("Start server failed:" + err.Error())
+		return
+	}
+	defer linster.Close()
+
+	fmt.Println("Start zserver ok on port" + PORT)
+
+	for {
+		conn, err := linster.Accept()
+		if err != nil {
+			fmt.Println("Accept failed:" + err.Error())
+			continue
+		}
+		go netHandler(conn)
+	}
+}
+
+func netHandler(conn net.Conn) {
+	defer conn.Close()
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("Net handler fault:%s", r)
+		}
+	}()
+
+	readData := make([]byte, 0)
+	n, err := conn.Read(readData)
+	if err != nil {
+		fmt.Println("Read Failed:")
+	}
+	fmt.Println("Read data ok" + string(n))
 }
