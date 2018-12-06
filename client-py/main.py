b4a258f9faa729442ba7b6216d0fffe035929acc
zhoujinlu <47242868@163.com>
2018-12-06 10:49:40 +0800

    add cpp server

--- client-py/main.py
+++ client-py/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+import socket
+
+print("Start client for python")
+
+try:
+    client = socket.socket()
+    client.connect(("127.0.0.1", 8085))
+    client.send("hello world".encode())
+    client.close()
+except Exception as e:
+    print("Client fault:" + str(e))
